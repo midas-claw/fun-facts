a45190341293acd2ef73b0d7399699a51075d02d
midas_claw <edragon.rocker@gmail.com>
2015-09-26 18:09:47 -0400

    Initial commit

---
+++ app/src/main/java/com/smartdragons/funfacts/FunFactsActivity.java
@@ -0,0 +1,46 @@
+package com.smartdragons.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        // Declare our View variables and assign them the views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = FactBook.getFact();
+                // Update the label with our dynamic fact
+                factLabel.setText(fact);
+
+                int color = ColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        Toast.makeText(this, "Yay! Our activity was created!", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "We're logging from the onCreate() method!");
+    }
+
+}
